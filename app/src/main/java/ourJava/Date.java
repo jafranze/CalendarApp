7a529f9b85ec460d66b389a3beccbb98b36eaff6
jafranze <jfranze0226@gmail.com>
2014-11-12 21:04:36 -0600

    new date class

--- app/src/main/java/ourJava/Date.java
+++ app/src/main/java/ourJava/Date.java
@@ -8,14 +8,40 @@ public class Date
     int YEAR;
     int MONTH;
     int DAY;
+	int HOURS;
     int MINUTES;
     int SECONDS;
-    public Date(int year, int month, int day, int minutes, int seconds)
+    public Date(int year, int month, int day, int hours, int minutes, int seconds)
     {
         YEAR = year;
         MONTH = month;
         DAY = day;
+		HOURS = hours;
         MINUTES = minutes;
         SECONDS = seconds;
     }
+	public int getYear()
+	{
+		return YEAR;
+	}
+	public int getMonth()
+	{
+		return MONTH;
+	}
+	public int getDay()
+	{
+		return DAY;
+	}
+	public int getHours()
+	{
+		return HOURS;
+	}
+	public int getMinutes()
+	{
+		return MINUTES;
+	}
+	public int getSeconds()
+	{
+		return SECONDS;
+	}
 }
